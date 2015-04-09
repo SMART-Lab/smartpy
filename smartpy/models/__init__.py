dfbfd45427172209a0e92d18e504c28d101b2ce7
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2015-04-09 14:51:00 -0400

    Fixed NestedTrained

--- smartpy/models/__init__.py
+++ smartpy/models/__init__.py
@@ -18,10 +18,19 @@ def factory(model_name, input_size, hyperparams):
         from smartpy.models.nade import NADE
         from smartpy.models.nested_nade import NestedNADE
         trained_nade = NADE.create(hyperparams["nade"])
+
+        hidden_size = hyperparams["size"]
+        if hidden_size is None:
+            hidden_size = trained_nade.hyperparams["hidden_size"]
+
+        hidden_activation = hyperparams["hidden_activation"]
+        if hidden_activation is None:
+            hidden_activation = trained_nade.hyperparams["hidden_activation"]
+
         model = NestedNADE(input_size=input_size,
-                           hidden_size=hyperparams["size"],
+                           hidden_size=hidden_size,
                            trained_nade=trained_nade,
-                           hidden_activation=hyperparams["hidden_activation"],
+                           hidden_activation=hidden_activation,
                            gamma=hyperparams["gamma"]
                            )
 
