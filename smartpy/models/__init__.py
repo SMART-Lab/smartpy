25d5f18d063522b1d4c2547fe5ae382a298db1a6
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2015-04-14 00:19:31 -0400

    Forgot some modif

--- smartpy/models/__init__.py
+++ smartpy/models/__init__.py
@@ -15,22 +15,10 @@ def factory(model_name, input_size, hyperparams):
                      #regularization=regularization_method
                      )
     elif model_name.lower() == "NestedNADE".lower():
-        from smartpy.models.nade import NADE
         from smartpy.models.nested_nade import NestedNADE
-        trained_nade = NADE.create(hyperparams["nade"])
-
-        hidden_size = hyperparams["size"]
-        if hidden_size is None:
-            hidden_size = trained_nade.hyperparams["hidden_size"]
-
-        hidden_activation = hyperparams["hidden_activation"]
-        if hidden_activation is None:
-            hidden_activation = trained_nade.hyperparams["hidden_activation"]
-
-        model = NestedNADE(input_size=input_size,
-                           hidden_size=hidden_size,
-                           trained_nade=trained_nade,
-                           hidden_activation=hidden_activation,
+        model = NestedNADE(trained_nade=hyperparams["nade"],
+                           hidden_size=hyperparams["size"],
+                           hidden_activation=hyperparams["hidden_activation"],
                            gamma=hyperparams["gamma"]
                            )
 
